94c91f0146c5f7f1d8811250a3074064d0c9c1dc
Kartik <ksihotiya@gmail.com>
2021-10-23 00:50:52 +0530

    Added Register page

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,18 @@
-
 import './App.css';
 import Header from './Components/Header/Header';
+import Register from "./Page/Register/Register"
+import Login from "./Page/Login/Login"
+import {BrowserRouter, Switch, Route} from "react-router-dom"
 function App() {
   return (
     <div className="App">
-      <Header />
+      <BrowserRouter>
+      <Switch>
+        <Route exact path="/" component={Header} />
+        <Route path="/register" component={Register} />
+        <Route path="/login" component={Login} />
+      </Switch>
+      </BrowserRouter>
     </div>
   );
 }
---
+++ client/src/Page/Register/Register.js
@@ -0,0 +1,25 @@
+import React from 'react'
+import "./Register.css"
+function Register() {
+    return (
+        <div className="register">
+            <div className="register-left">
+            <h1>SocialBook</h1>
+            <p>Connect with friends and the world around you on SocialBook</p>
+            </div>
+            <div className="register-right">
+            <input type="text" placeholder="Username" />
+            <input type="text" placeholder="Email"  />
+            <input type="text" placeholder="Password"  />
+            <input type="text" placeholder="Confirm Password" />
+            <button className="loginButton">Sign Up</button>
+            <button className="loginRegisterButton">
+              Log into Account
+            </button>   
+            </div>
+            
+        </div>
+    )
+}
+
+export default Register
