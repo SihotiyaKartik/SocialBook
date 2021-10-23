efd7feec698fbe2ab203a92027c4b23c24314ce2
Kartik <ksihotiya@gmail.com>
2021-10-23 18:21:50 +0530

    Added post in feed.js

--- client/src/Components/Feed/Feed.js
+++ client/src/Components/Feed/Feed.js
@@ -1,11 +1,12 @@
 import React from 'react'
 import Upload from '../Upload/Upload'
-
+import Post from '../Post/Post'
 import "./Feed.css"
 function Feed() {
     return (
         <div className="feed">
             <Upload />
+            <Post />
         </div>
     )
 }
